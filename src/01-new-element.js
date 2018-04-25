402db7743bb6378006d73213aabae2c6a8665295
David Mallard <correspondence@davidmallard.id.au>
2018-04-25 16:49:23 +1000

    Configure for multiple entry points with an index page and a D3 demo

---
+++ src/01-new-element.js
@@ -0,0 +1,6 @@
+import * as d3 from 'd3';
+
+d3
+  .select('body')
+  .append('p')
+  .text('New paragraph!');
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,26 @@
+const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  entry: {
+    index: './src/index.js',
+    new_element: './src/01-new-element.js'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
-      template: __dirname + '/public/index.html'
+      template: __dirname + '/public/index.html',
+      chunks: ['index']
+    }),
+    new HtmlWebpackPlugin({
+      filename: '01-new-element.html',
+      template: __dirname + '/public/01-new-element.html',
+      chunks: ['new_element']
     })
   ],
   module: {
