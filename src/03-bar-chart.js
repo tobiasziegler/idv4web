b40e93769025d1506f5a8c0fb4d5ba6518b82cdc
David Mallard <correspondence@davidmallard.id.au>
2018-04-30 15:25:32 +1000

    Add many more data values to the bar chart

--- src/03-bar-chart.js
+++ src/03-bar-chart.js
@@ -1,7 +1,33 @@
 import * as d3 from 'd3';
 import './03-bar-chart.css';
 
-const dataset = [25, 7, 5, 26, 11];
+const dataset = [
+  25,
+  7,
+  5,
+  26,
+  11,
+  8,
+  25,
+  14,
+  23,
+  19,
+  14,
+  11,
+  22,
+  29,
+  11,
+  13,
+  12,
+  17,
+  18,
+  10,
+  24,
+  18,
+  25,
+  9,
+  3
+];
 
 d3
   .select('body')
