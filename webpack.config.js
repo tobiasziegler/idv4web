94cfef7b098cf79a723f379272077b92ef76ead5
David Mallard <correspondence@davidmallard.id.au>
2018-04-30 15:14:45 +1000

    Add support for bundling CSS files to the project

--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,10 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
       }
     ]
   },
