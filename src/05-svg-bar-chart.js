ccc9bdf44cb426fef87296098adee9c886139aa3
David Mallard <correspondence@davidmallard.id.au>
2018-05-01 15:09:10 +1000

    Create the initial SVG bar chart with static positioning and height bars

---
+++ src/05-svg-bar-chart.js
@@ -0,0 +1,45 @@
+import * as d3 from 'd3';
+
+const dataset = [
+  5,
+  10,
+  13,
+  19,
+  21,
+  25,
+  22,
+  18,
+  15,
+  13,
+  11,
+  12,
+  15,
+  20,
+  18,
+  17,
+  16,
+  18,
+  23,
+  25
+];
+
+// Width and height
+const w = 500;
+const h = 100;
+
+// Create SVG element
+const svg = d3
+  .select('body')
+  .append('svg')
+  .attr('width', w)
+  .attr('height', h);
+
+svg
+  .selectAll('rect')
+  .data(dataset)
+  .enter()
+  .append('rect')
+  .attr('x', 0)
+  .attr('y', 0)
+  .attr('width', 20)
+  .attr('height', 100);
--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,8 @@ module.exports = {
     new_element: './src/01-new-element.js',
     data: './src/02-data.js',
     div_bar_chart: './src/03-div-bar-chart.js',
-    circles: './src/04-circles.js'
+    circles: './src/04-circles.js',
+    svg_bar_chart: './src/05-svg-bar-chart.js'
   },
   output: {
     filename: '[name].bundle.js',
@@ -39,6 +40,11 @@ module.exports = {
       filename: '04-circles.html',
       template: __dirname + '/public/04-circles.html',
       chunks: ['circles']
+    }),
+    new HtmlWebpackPlugin({
+      filename: '05-svg-bar-chart.html',
+      template: __dirname + '/public/05-svg-bar-chart.html',
+      chunks: ['svg_bar_chart']
     })
   ],
   module: {
