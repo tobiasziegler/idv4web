e302e2614362e3c139a858f695523029d896f8ad
David Mallard <correspondence@davidmallard.id.au>
2018-05-03 17:13:28 +1000

    Modify the bar chart to use keyed data so bars are removed correctly

--- src/05-svg-bar-chart.js
+++ src/05-svg-bar-chart.js
@@ -1,27 +1,29 @@
 import * as d3 from 'd3';
 
 let dataset = [
-  5,
-  10,
-  13,
-  19,
-  21,
-  25,
-  22,
-  18,
-  15,
-  13,
-  11,
-  12,
-  15,
-  20,
-  18,
-  17,
-  16,
-  18,
-  23,
-  25
+  { key: 0, value: 5 },
+  { key: 1, value: 10 },
+  { key: 2, value: 13 },
+  { key: 3, value: 19 },
+  { key: 4, value: 21 },
+  { key: 5, value: 25 },
+  { key: 6, value: 22 },
+  { key: 7, value: 18 },
+  { key: 8, value: 15 },
+  { key: 9, value: 13 },
+  { key: 10, value: 11 },
+  { key: 11, value: 12 },
+  { key: 12, value: 15 },
+  { key: 13, value: 20 },
+  { key: 14, value: 18 },
+  { key: 15, value: 17 },
+  { key: 16, value: 16 },
+  { key: 17, value: 18 },
+  { key: 18, value: 23 },
+  { key: 19, value: 25 }
 ];
+
+const key = d => d.key;
 
 // Width and height
 const w = 600;
@@ -35,7 +37,7 @@ const xScale = d3
 
 const yScale = d3
   .scaleLinear()
-  .domain([0, d3.max(dataset)])
+  .domain([0, d3.max(dataset, d => d.value)])
   .range([0, h]);
 
 // Create SVG element
@@ -47,26 +49,26 @@ const svg = d3
 
 svg
   .selectAll('rect')
-  .data(dataset)
+  .data(dataset, key)
   .enter()
   .append('rect')
   .attr('x', (d, i) => xScale(i))
-  .attr('y', d => h - yScale(d))
+  .attr('y', d => h - yScale(d.value))
   .attr('width', xScale.bandwidth())
-  .attr('height', d => yScale(d))
-  .attr('fill', d => 'rgb(0, 0, ' + Math.round(d * 10) + ')');
+  .attr('height', d => yScale(d.value))
+  .attr('fill', d => 'rgb(0, 0, ' + Math.round(d.value * 10) + ')');
 
 svg
   .selectAll('text')
-  .data(dataset)
+  .data(dataset, key)
   .enter()
   .append('text')
-  .text(d => d)
+  .text(d => d.value)
   .attr('x', (d, i) => xScale(i) + xScale.bandwidth() / 2)
-  .attr('y', d => h - yScale(d) + 14)
+  .attr('y', d => h - yScale(d.value) + 14)
   .attr('font-family', 'sans-serif')
   .attr('font-size', '11px')
-  .attr('fill', d => (d > 1 ? 'white' : 'black'))
+  .attr('fill', d => (d.value > 1 ? 'white' : 'black'))
   .attr('text-anchor', 'middle');
 
 d3.select('p').on('click', () => {
@@ -75,54 +77,59 @@ d3.select('p').on('click', () => {
 
   // Update scale domains
   xScale.domain(d3.range(dataset.length));
-  yScale.domain([0, d3.max(dataset)]);
+  yScale.domain([0, d3.max(dataset, d => d.value)]);
 
   // Select...
-  const bars = svg.selectAll('rect').data(dataset);
+  const bars = svg.selectAll('rect').data(dataset, key);
 
   // Enter...
   bars
     .enter()
     .append('rect')
     .attr('x', w)
-    .attr('y', d => h - yScale(d))
+    .attr('y', d => h - yScale(d.value))
     .attr('width', xScale.bandwidth())
-    .attr('height', d => yScale(d))
-    .attr('fill', d => 'rgb(0, 0, ' + Math.round(d * 10) + ')')
+    .attr('height', d => yScale(d.value))
+    .attr('fill', d => 'rgb(0, 0, ' + Math.round(d.value * 10) + ')')
     .merge(bars)
     .transition()
     .duration(500)
     .attr('x', (d, i) => xScale(i))
-    .attr('y', d => h - yScale(d))
+    .attr('y', d => h - yScale(d.value))
     .attr('width', xScale.bandwidth())
-    .attr('height', d => yScale(d))
-    .attr('fill', d => 'rgb(0, 0, ' + Math.round(d * 10) + ')');
+    .attr('height', d => yScale(d.value));
 
   // Exit
   bars
     .exit()
     .transition()
     .duration(500)
-    .attr('x', w)
+    .attr('x', -xScale.bandwidth())
     .remove();
 
   // Select...
-  const labels = svg.selectAll('text').data(dataset);
+  const labels = svg.selectAll('text').data(dataset, key);
 
   // Enter...
   labels
     .enter()
     .append('text')
     .attr('x', w + xScale.bandwidth() / 2)
-    .attr('y', d => (d > 7 ? h - yScale(d) + 14 : h - yScale(d) - 3))
-    .attr('fill', d => (d > 1 ? 'white' : 'black'))
+    .attr(
+      'y',
+      d => (d.value > 7 ? h - yScale(d.value) + 14 : h - yScale(d.value) - 3)
+    )
+    .attr('fill', d => (d.value > 1 ? 'white' : 'black'))
     .merge(labels)
     .transition()
     .duration(500)
-    .text(d => d)
+    .text(d => d.value)
     .attr('x', (d, i) => xScale(i) + xScale.bandwidth() / 2)
-    .attr('y', d => (d > 1 ? h - yScale(d) + 14 : h - yScale(d) - 3))
-    .attr('fill', d => (d > 1 ? 'white' : 'black'))
+    .attr(
+      'y',
+      d => (d.value > 1 ? h - yScale(d.value) + 14 : h - yScale(d.value) - 3)
+    )
+    .attr('fill', d => (d.value > 1 ? 'white' : 'black'))
     .attr('font-family', 'sans-serif')
     .attr('font-size', '11px')
     .attr('text-anchor', 'middle');
@@ -132,6 +139,6 @@ d3.select('p').on('click', () => {
     .exit()
     .transition()
     .duration(500)
-    .attr('x', w + xScale.bandwidth() / 2)
+    .attr('x', -xScale.bandwidth() / 2)
     .remove();
 });
