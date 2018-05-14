9f5ab19f0e768bf0e43dfbbc344c4ba73ce07b60
David Mallard <correspondence@davidmallard.id.au>
2018-05-14 16:46:27 +1000

    Enable source maps in development mode

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,7 +2,9 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
+  devtool: 'eval-source-map',
   devServer: {
+    contentBase: './dist',
     open: true,
     overlay: true
   }
